dc9e1a27847ba6454dc88fd7aa5d63ae37e1c911
shusaku009 <shusaku009@gmail.com>
2023-10-20 20:54:34 +0900

    Add: ライクボタンを作成

--- like-button/src/App.tsx
+++ like-button/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,20 +6,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App">
+          <header className="App-header">
+            <LikeButton />
+          </header>
+        </div>
       </header>
     </div>
+  );
+}
+
+function LikeButton() {
+  const [count, setCount] = useState(999);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <span className="likeButton" onClick={handleClick}>
+      ❤︎ {count}
+    </span>
   );
 }
 
